eaf65db84c21444eb669a7d7c985004dedd0e8ac
Paul Richardson <developer@photographerafoot.com>
2021-06-06 12:34:22 +1000

    CHG: make access public

--- Sources/StringExtensions/StringExtensions.swift
+++ Sources/StringExtensions/StringExtensions.swift
@@ -2,15 +2,15 @@ import Foundation
 
 extension String {
 
-	var isNumeric: Bool {
+	public var isNumeric: Bool {
 		return Double(self) != nil
 	}
 
-	var lines: [String] {
+	public var lines: [String] {
 		return self.components(separatedBy: .newlines)
 	}
 
-	func withPrefix(_ prefix: String) -> String {
+	public func withPrefix(_ prefix: String) -> String {
 		return self.hasPrefix(prefix) ? self : prefix + self
 	}
 	
